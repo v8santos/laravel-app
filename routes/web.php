86806832e48b3290fd0c745e3c9c723d45715105
Victor Santos <victor.engsantos@gmail.com>
2021-02-21 10:32:20 -0300

    Revert "Alteração rotas 02"
    This reverts commit 00e35980df8f9ce13720a20cd84c8b19072ab41c.

--- routes/web.php
+++ routes/web.php
@@ -2,40 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', function () {
-    return 'login page';
-})->name('login');
-
-Route::group([
-    'middleware' => [],
-    'prefix' => 'admin',
-    'namespace' => 'Admin',
-], function () {
-    Route::get('/profile', 'TesteController@teste')->name('admin.profile');
-                
-    Route::get('/dashboard', 'TesteController@teste')->name('admin.dashboard');
-
-    Route::get('/', function () {
-        return redirect()->route('admin.dashboard') ;
-    })->name('admin.home');
-});
-
-Route::get('/log-user', function () {
-    return redirect()->route('user.name');
-})->middleware('auth');
-
-Route::get('/name-user', function () {
-    return 'name-user';
-})->name('user.name');
-
-Route::view('/home', 'welcome');
-
-Route::redirect('/redirect1', '/redirect2');
-
-// Route::get('/redirect1', function () {
-//     return redirect('/redirect2');
-// });
-
 Route::get('/redirect2', function () {
     return 'redirect2';
 });
